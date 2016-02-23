2c636f9650a5e15b41ed2837eb897b08b79b0d27
Henrique Moody <henriquemoody@gmail.com>
2016-02-23 13:44:55 -0300

    Run PHP-CS-Fixer on codebase

--- src/Canvas/DrawContextInterface.php
+++ src/Canvas/DrawContextInterface.php
@@ -1,18 +1,19 @@
 <?php
+
 namespace GameBoy\Canvas;
 
 /**
  * Interface to draw the GameBoy output
- * GameBoy screen size: 160 x 144
+ * GameBoy screen size: 160 x 144.
  */
 interface DrawContextInterface
 {
     /**
-     * Draw image on canvas
+     * Draw image on canvas.
      *
-     * @param  Array $canvasBuffer  Each pixel => 4 items on array (RGBA)
-     * @param  int $left
-     * @param  int $top
+     * @param array $canvasBuffer Each pixel => 4 items on array (RGBA)
+     * @param int   $left
+     * @param int   $top
      */
     public function draw($canvasBuffer, $left, $top);
 }
