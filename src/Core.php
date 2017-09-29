6d989dd0c3f7205ed3e986c6b2bf141f0ff22d12
veasey <clintvee@gmail.com>
2017-09-29 15:24:13 +0100

    stick timing vars in a trait

--- src/Core.php
+++ src/Core.php
@@ -191,30 +191,8 @@ class Core
     //Timing Variables
     //
 
-    //Used to sample the audio system every x CPU instructions.
-    public $audioTicks = 0;
-
-    //Times for how many instructions to execute before ending the loop.
-    public $emulatorTicks = 0;
-
-    // DIV Ticks Counter (Invisible lower 8-bit)
-    public $DIVTicks = 14;
-
-    // ScanLine Counter
-    public $LCDTicks = 15;
-
-    // Timer Ticks Count
-    public $timerTicks = 0;
-
-    // Timer Max Ticks
-    public $TACClocker = 256;
-
-    //Are the interrupts on queue to be enabled?
-    public $untilEnable = 0;
-
-    //The last time we iterated the main loop.
-    public $lastIteration = 0;
-
+    use Config\TimingTrait;
+    
     //
     //ROM Cartridge Components:
     //
